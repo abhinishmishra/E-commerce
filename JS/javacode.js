ea6fc49ed681d7870364d3ddc2e9566c004505aa
Abhinish <abhinishm@gmail.com>
2024-07-25 22:04:33 +0530

    ShopEase

---
+++ JS/javacode.js
@@ -0,0 +1,57 @@
+// const favmovie="kgf";
+
+
+
+// let guess=prompt("Guess My Favourite Movie");
+
+// while((guess!=favmovie) && (guess!="quite")){
+//     guess=prompt('wrong guess, Please try again')
+// }
+// if(guess=="kgf"){
+//     alert("CORRECT");
+// }
+// else if(guess=="quite"){
+//     alert("you quit!");
+    
+// }
+// else{
+//     alert("INCORRECT");
+// }
+
+
+// function
+// function greet(){
+//     console.log("hello");
+// }
+// greet()
+// greet()
+// greet()
+
+// function print(){
+//     for(i=1; i<=10;i++){
+//         console.log(i);
+//     }
+// }
+// print();
+
+
+function greet(name){
+    console.log(`hi! ${name}` );
+}
+greet("abhinish");
+greet("hi aditya")
+
+function sum(a,b){
+    return a+b;
+}
+console.log(sum(22,9));
+
+
+// let sum=34;
+
+// function calsum(a,b){
+//     let sum= a+b;
+//     console.log(sum);
+// }
+// calsum(2,3);
+// console.log (sum);
---
+++ app.js
@@ -0,0 +1,96 @@
+console.log("Hello World");
+console.log("This is my First Program");
+// let age;
+// if (age>18){
+//     console.log("person can vote");
+// }
+// else {
+//     console.log("person can't vote" )
+// }
+
+// alert("welcom to our website");
+
+// prompt("Enter Value of Age");
+// console.warn("warning");
+// console.error("warning");
+
+//nested loop
+// let marks=98;
+// if(marks>90){
+//     if(marks>95){
+//     console.log("outstanding");
+//     }
+//    else {
+//         console.log("Excellent");
+//     }
+// }
+// else{
+//     console.log("keep it up!")
+// }
+
+//else if statement
+// let mark =93;
+// if (mark>79 && marks<80){
+//     console.log("Keep it up!");
+// }
+// else if(marks>80 && marks<90){
+//     console.log("Good");
+// }
+// else if(mark>90 && mark<95){
+//     console.log("Excellent");
+// }
+// else{
+//     console.log("Outstanding");
+// }
+
+//switch statement
+// let color="orange";
+// switch(color){
+//     case'red': console.log("Stop");
+//     break;
+//     case "yellow" : console.log("wait");
+//     break;
+//     case "blue": console.log("run");
+//     break;
+//     case "orange": console.log("back");
+//     break;
+
+// }
+// for(i = 1; i<=10; i+=2){
+//     console.log(i);
+// }
+//even no
+for(i=2; i<=10; i+=2){
+    console.log(i);
+}
+//break statement
+for(i=1;i<10;i++){
+    if(i==5){
+        break;
+    }
+    console.log(i);
+}
+
+//continous 
+for(i=1;i<=10;i++){
+    if(i%2==0){
+        continue
+    }
+    console.log(i);
+}
+//*****
+//*****
+//***** 
+
+// for(i=0;i<=3;i++){
+//     for(j=0;j<=3;j++){
+//         console.log("*");
+//     }
+    
+// }
+
+let i=1;
+while(i<10){
+    console.log(i);
+    i++;
+}
